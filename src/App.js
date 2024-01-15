d01ffb67eb1f0a996b995cf006b1557d86d68482
jonataslaet <jonataslaetprogramador@gmail.com>
2024-01-15 17:04:54 -0300

    Testando

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>Mangekyou Sharingan!</p>
     </div>
   );
 }
